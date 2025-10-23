d3cca9eac43bcfc50a2e12af233bd0d798131f83
Krzysztof Witucki <n3rsti@protonmail.com>
2025-10-23 10:04:11 +0200

    create program model

---
+++ app/models/program.model.ts
@@ -0,0 +1,60 @@
+export class Program {
+    constructor(
+        public id: number,
+        public name: string,
+        public duration: number,
+        public description: string,
+        public language: string,
+        public image: string,
+    ) {}
+}
+
+export class ProgramBuilder {
+    private id!: number;
+    private name!: string;
+    private duration!: number;
+    private description!: string;
+    private language!: string;
+    private image!: string;
+
+    setId(id: number): this {
+        this.id = id;
+        return this;
+    }
+
+    setName(name: string): this {
+        this.name = name;
+        return this;
+    }
+
+    setDuration(duration: number): this {
+        this.duration = duration;
+        return this;
+    }
+
+    setDescription(description: string): this {
+        this.description = description;
+        return this;
+    }
+
+    setLanguage(language: string): this {
+        this.language = language;
+        return this;
+    }
+
+    setImage(image: string): this {
+        this.image = image;
+        return this;
+    }
+
+    build(): Program {
+        return new Program(
+            this.id,
+            this.name,
+            this.duration,
+            this.description,
+            this.language,
+            this.image,
+        );
+    }
+}
