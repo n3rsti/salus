4abcac23c803eab455a04298c11b02c8d76643c5
Krzysztof Witucki <n3rsti@protonmail.com>
2025-10-24 22:04:02 +0200

    add AppProgramCard component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,75 +10,11 @@
         <section
             class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3"
         >
-            <AppCard
+            <AppProgramCard
                 v-for="program in programs"
                 :key="program.id"
-                :image="program.image"
-                :title="program.name"
-                :description="program.description"
-            >
-                <div
-                    v-if="program.progress"
-                    class="flex items-center justify-between mt-auto"
-                >
-                    <p
-                        v-if="program.duration"
-                        class="font-medium text-muted/90 text-sm flex items-center gap-1"
-                    >
-                        <Icon name="ic:baseline-calendar-month" />
-                        Day
-                        {{
-                            faker.number.int({
-                                min: 1,
-                                max: program.duration,
-                            })
-                        }}/{{ program.duration }}
-                    </p>
-                    <p class="text-sm flex items-center text-green-500 gap-1">
-                        {{ program.progress }}%
-                    </p>
-                </div>
-
-                <div v-else class="flex items-center justify-between mt-auto">
-                    <p class="text-muted/90 text-sm flex items-center gap-1">
-                        <Icon name="ic:outline-access-time" />
-                        {{ program.duration }}
-                        days
-                    </p>
-                    <p class="text-sm flex items-center text-text gap-1">
-                        {{ program.rating || 0 }}/5
-                        <Icon
-                            name="material-symbols:star-rounded"
-                            class="text-yellow-400 text-2xl"
-                        />
-                    </p>
-                </div>
-
-                <progress
-                    v-if="program.progress"
-                    class="progress progress-success w-full mt-4"
-                    :value="program.progress"
-                    max="100"
-                ></progress>
-
-                <AppButton
-                    v-if="program.progress"
-                    class="w-full shadow-sm border-t-transparent text-white mt-3"
-                    :color="'green'"
-                >
-                    Continue
-
-                    <Icon class="text-lg ml-2" name="ic:round-play-arrow" />
-                </AppButton>
-                <AppButton
-                    v-else
-                    class="w-full shadow-sm border-t-transparent text-white mt-3"
-                    :color="'green_dark'"
-                >
-                    View info
-                    <Icon class="text-lg ml-2" name="ic:round-remove-red-eye" />
-                </AppButton>
-            </AppCard>
+                :program="program"
+            />
             <AppCard
                 v-for="activity in activities"
                 :key="activity.id"
