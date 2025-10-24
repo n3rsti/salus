42cf4a0b89aa66dca4f872e098f37041601165c3
Krzysztof Witucki <n3rsti@protonmail.com>
2025-10-24 19:49:03 +0200

    add .env loading for database

--- api/database.py
+++ api/database.py
@@ -1,16 +1,28 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
+from dotenv import dotenv_values
 
-postgresql_database_url = "postgresql://postgres:admin@localhost:5432/salus"
+config = dotenv_values(".env")
+
+user = config.get("POSTGRES_USER", "postgres")
+password = config.get("POSTGRES_PASSWORD", "admin")
+db = config.get("POSTGRES_DB", "salus")
+host = config.get("POSTGRES_HOST", "localhost")
+
+postgresql_database_url = f"postgresql://{user}:{password}@{host}:5432/{db}"
 engine = create_engine(postgresql_database_url, echo=True)
+
 
 def create_db_and_tables():
     # SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
 
+
 def get_session():
     with Session(engine) as session:
         yield session
 
+
 SessionDep = Annotated[Session, Depends(get_session)]
+
