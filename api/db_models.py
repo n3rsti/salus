f6a5c4719b35cb368c3ed2a29cd1e5c6676a6652
Kacper <malakkacper64@gmail.com>
2025-10-24 18:51:12 +0200

    more endpoints for activities

--- api/db_models.py
+++ api/db_models.py
@@ -1,4 +1,5 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
 
 class Activity(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -6,9 +7,28 @@ class Activity(SQLModel, table=True):
     duration_minutes: int
     description: str
     difficulty: int
+    media: List["ActivityMedia"] = Relationship(back_populates="activity", cascade_delete=True, )
 
 class ActivityMedia(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     url: str
     type: str
-    activity_id: int = Field(foreign_key="activity.id")
+
+    activity_id: int | None = Field(default=None, foreign_key="activity.id", ondelete="CASCADE")
+    activity: Activity | None = Relationship(back_populates="media")
+
+class ActivityMediaRead(SQLModel):
+    id: int
+    url: str
+    type: str
+    activity_id: int | None = None
+
+class ActivityRead(SQLModel):
+    id: int
+    name: str
+    duration_minutes: int
+    description: str
+    difficulty: int
+    media: List[ActivityMediaRead] = []
+
+
