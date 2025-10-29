5e381ed067e06ea8ebf9e12b1a83da3edfab718c
Kacper <malakkacper64@gmail.com>
2025-10-29 11:32:14 +0100

    CORS changed

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_db_and_tables
 from routers import activity_router, program_router, tag_router
 
@@ -11,3 +12,11 @@ def on_startup():
 app.include_router(activity_router.router)
 app.include_router(program_router.router)
 app.include_router(tag_router.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #change it, when * it is not secure !!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
